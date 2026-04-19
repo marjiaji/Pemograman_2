6a5c3052ca9ca14d89d11b4fd5a3e8777da67153
marji <143249402+marjiaji@users.noreply.github.com>
2026-04-19 20:26:04 +0700

    pertemuan 6

---
+++ pemograman2pert6/src/Koneksi.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Koneksi {
+    Connection con;
+
+    public Connection getKoneksi() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/db_pertemuan6",
+                "root",
+                ""
+            );
+
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception e) {
+            System.out.println("Koneksi Gagal : " + e.getMessage());
+        }
+        return con;
+    }
+}
